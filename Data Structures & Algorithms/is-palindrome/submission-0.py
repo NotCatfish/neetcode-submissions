5a92fd1f70382ed507a73f27575a4edba5d6fd09
アリス <indraneelsamanta2005@gmail.com>
2026-08-02 16:58:09 +0530

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -1,12 +1,8 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-
-        #clean s and keep only alpha or num delete rest and make it single string
-        clean_s = ''.join(char.lower() for char in s if char.isalnum())
-
-
-        #compare front and rear liek a 2pointer return true if same else dfalse
-        if clean_s==clean_s[::-1]:
+        cleaned = re.sub(r'[^a-zA-Z0-9]', '', s)
+        cleaned=cleaned.lower()
+        if cleaned==cleaned[::-1]:
             return True
         else:
             return False
