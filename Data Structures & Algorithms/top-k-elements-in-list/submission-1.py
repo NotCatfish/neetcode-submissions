d7fe17e252cd0ecf328222afbc05bf441d366925
アリス <indraneelsamanta2005@gmail.com>
2026-08-09 10:16:32 +0530

    Update: top-k-elements-in-list - submission-1

--- Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -1,15 +1,4 @@
+import pandas as pd
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        store={}
-        
-        for x in nums:
-            if x in store:
-                store[x]+=1
-            else:
-                store[x]=1
-
-        unique_elements = list(store.keys())
-        unique_elements.sort(key=lambda x: store[x], reverse=True)
-        top = unique_elements[:k]
-
-        return top
+        return pd.Series(nums).value_counts().head(k).index.tolist()
