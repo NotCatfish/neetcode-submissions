0a10be808f1d62e4bb98b7d500561619ddabeaed
アリス <indraneelsamanta2005@gmail.com>
2026-09-09 22:14:51 +0530

    Update: binary-search - submission-1

--- Data Structures & Algorithms/binary-search/submission-1.py
+++ Data Structures & Algorithms/binary-search/submission-1.py
@@ -3,12 +3,13 @@ class Solution:
         left=0
         right=len(nums)-1
         while left<=right:
-            mid=left+(right-left)//2
+            mid=(left + right)//2
             if nums[mid]==target:
                 return mid
-            elif target<nums[mid]:
+            elif nums[mid]>target:
                 right=mid-1
             else:
                 left=mid+1
+            
         
         return -1
